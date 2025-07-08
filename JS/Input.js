c792f4c782fcda0130125cc73d61d9baf9372556
Pr8222 <parsa.rohani82@gmail.com>
2025-07-08 14:47:26 +0330

    relocationed the bootstrap icons folder and fixed some bugs like the bottom positioning and the icons positionings. There was a js issue and now it's ok

--- JS/Input.js
+++ JS/Input.js
@@ -7,8 +7,10 @@ $(document).ready(function () {
 
     var selectedOption = $(this).val();
     if (selectedOption == "Iranian") {
-      $(`<div class="form mb-3 me-5" id="residenceDisplay">
-                    <select class="form-select form-select-lg mb-3 text-body-secondary fs-6" id="residence">
+      $(`<div class="form mb-3 me-5 position-relative" id="residenceDisplay">
+                    <i class="bi bi-layers position-absolute top-50 translate-middle-y me-3 text-secondary"
+                        style="right: 0.75rem; font-size: 18px;"></i> 
+                    <select class="form-select form-select-lg mb-3 ps-5 text-body-secondary fs-6" id="residence">
                         <option selected>نوع اقامت</option>
                         <option value="Resident">ساکنین</option>
                         <option value="Employed">شاغلین</option>
@@ -27,11 +29,13 @@ $(document).ready(function () {
 
     if (selectedOption == "Employed") {
       $(`
-            <div class="form mb-3 me-5" id="professionKindDisplay">
-                <select class="form-select form-select-lg mb-3 text-body-secondary fs-6" id="professionKind">
-                    <option selected>انتخاب نوع شاغلین</option>
-                    <option value="FreeZone">کارکنان مناطق آزاد و دستگاه‌های اجرایی</option>
-                    <option value="Others">سایر</option>
+            <div class="form mb-3 me-5 position-relative" id="professionKindDisplay">
+                <i class="bi bi-layers position-absolute top-50 translate-middle-y me-3 text-secondary"
+                        style="right: 0.75rem; font-size: 18px;"></i>
+                <select class="form-select form-select-lg mb-3 ps-5 text-body-secondary fs-6" id="professionKind">
+                    <option selected><i class="bi bi-layers"></i>انتخاب نوع شاغلین</option>
+                    <option value="FreeZone"><i class="bi bi-layers"></i>کارکنان مناطق آزاد و دستگاه‌های اجرایی</option>
+                    <option value="Others"><i class="bi bi-layers"></i>سایر</option>
                 </select>
             </div>
         `).insertAfter("#residenceDisplay");
@@ -42,12 +46,12 @@ $(document).ready(function () {
   $("#freeZone").change(function (e) {
     e.preventDefault();
 
-    $("#residenceDisplay").remove(); // Always clean up
+    $("#select-free-zone").remove(); // Always clean up
 
     var selectedOption = $(this).val();
     if (selectedOption == "FreeZone") {
-      $(`<div class="form mb-3 me-5" id="residenceDisplay">
-                    <select class="form-select form-select-lg mb-3 text-body-secondary fs-6" id="residence">
+      $(`<div class="form mb-3 me-5" id="select-free-zone-display">
+                    <select class="form-select form-select-lg mb-3 text-body-secondary fs-6" id="select-free-zone">
                         <option selected>انتخاب منطقه آزاد تجاری-صنعتی</option>
                         <option value="Ilam">منطقه آزاد تجاری-صنعتی ایلام</option>
                         <option value="Busher">منطقه آزاد تجاری-صنعتی بوشهر</option>
@@ -64,9 +68,9 @@ $(document).ready(function () {
     }
   });
 
-  $("#next-page").click(function (e) { 
+  $("#next-page").click(function (e) {
     e.preventDefault();
-    localStorage.setItem("phoneNumber",$("#phone-number").val());
+    localStorage.setItem("phoneNumber", $("#phone-number").val());
     document.location.href = "/phoneVerification.html";
   });
 });
